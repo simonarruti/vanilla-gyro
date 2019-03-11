41270b1828b01f4142b79d297ab0ddb714f32d70
sar <simon.arruti@1000mercis.com>
2019-03-11 16:10:18 +0100

    replace device orientation by device motion in test

--- public/app.js
+++ public/app.js
@@ -4,33 +4,52 @@ element.style.backgroundPositionY = "0px"
 
 const log = document.querySelector('#gyro_info')
 
-const speed = 1.5
+const speed = 1.7
+const factor = 0.7
 
 let lastX = 0
 let lastY = 0
 let lastStepX = 0
 let lastStepY = 0
-let orientations = {alpha: 0, beta: 0, gamma: 0}
+let orientations = {alpha: 0, beta: 0, gamma: 0, x: 0, y: 0, z: 0}
 
 if (window.DeviceOrientationEvent) {
   window.addEventListener("deviceorientation", function (event) {
     // console.log(event)
-    log.innerHTML = `<pre>
-Alpha: ${event.alpha} <br>
-Beta: ${event.beta} <br>
-Gamma: ${event.gamma} <br>
-</pre>`
-    orientations.alpha = event.alpha
-    orientations.beta = event.beta
-    orientations.gamma = event.gamma
+//     log.innerHTML = `<pre>
+// Alpha: ${event.alpha} <br>
+// Beta: ${event.beta} <br>
+// Gamma: ${event.gamma} <br>
+// </pre>`
+    orientations.alpha = Math.round(event.alpha)
+    orientations.beta = Math.round(event.beta)
+    orientations.gamma = Math.round(event.gamma)
   }, true)
 } else {
-  console.log('Not supported')
+  console.log('Device orientation not supported')
 }
 
+if (window.DeviceMotionEvent) {
+   window.addEventListener('devicemotion', function (event) {
+     orientations.x = Math.round(factor * orientations.x + (1 - factor) * event.accelerationIncludingGravity.x)
+     orientations.y = Math.round(factor * orientations.y + (1 - factor) * (event.accelerationIncludingGravity.y * -1))
+     orientations.z = Math.round(event.accelerationIncludingGravity.z)
+
+     log.innerHTML = `<pre>
+     X: ${orientations.x}
+     Y: ${orientations.y}
+     Z: ${orientations.z}
+</pre>`
+   })
+} else {
+  console.log('Deveice motion not supported')
+}
 
 // functions
 function getStepX (lastXPosition, currentXPosition, lastStep) {
+  if (currentXPosition === 0) {
+    return 0
+  }
   if (lastXPosition === currentXPosition) {
     return lastStep
   } else {
@@ -39,6 +58,9 @@ function getStepX (lastXPosition, currentXPosition, lastStep) {
 }
 
 function getStepY (lastYPosition, currentYPosition, lastStep) {
+  if (currentYPosition === 0) {
+    return 0
+  }
   if (lastYPosition === currentYPosition) {
     return lastStep
   } else {
@@ -61,22 +83,22 @@ function parsedBG (domElement, axe, intType, valueReplace = 'px') {
 
 function refresh () {
   const o = getOrientations()
-  const stepX = getStepX(lastX, o.alpha, lastStepX)
-  const stepY = getStepY(lastY, o.beta, lastStepY)
+  const stepX = getStepX(lastX, o.x, lastStepX)
+  const stepY = getStepY(lastY, o.y, lastStepY)
   console.log(stepY)
 
   if ((parsedBG(element, 'X', 'int') > ((window.innerWidth - element.offsetWidth) / 2) && stepX < 0) || (parsedBG(element, 'X', 'int') < ((element.offsetWidth - window.innerWidth) / 2) && stepX > 0)) {
-    element.style.backgroundPositionX = Math.round(parsedBG(element, 'X', 'float') + stepX) + 'px'
+    element.style.backgroundPositionX = Math.round(parsedBG(element, 'X', 'float') + stepX * speed) + 'px'
 
-    lastX = o.alpha
+    lastX = o.x
     lastStepX = stepX
     // console.log('X', element.style.backgroundPositionX)
   }
 
   if ((parsedBG(element, 'Y', 'int') > ((window.innerHeight - element.offsetHeight) / 2) && stepY < 0) || (parsedBG(element, 'Y', 'int') < ((element.offsetHeight - window.innerHeight) / 2)) && stepY > 0) {
-    element.style.backgroundPositionY = Math.round(parsedBG(element, 'Y', 'float') + stepY) + 'px'
+    element.style.backgroundPositionY = Math.round(parsedBG(element, 'Y', 'float') + stepY * speed) + 'px'
 
-    lastY = o.beta
+    lastY = o.y
     lastStepY = stepY
   }
 
